2d6524cf710afc3a99e8c5a3722d6c86167c118e
Student <jsitbon@qcc.cuny.edu>
2019-08-08 14:31:43 -0400

    Build

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -16,8 +16,8 @@ export class Home extends Component {
         <p>To help get started, we have also set up:</p>
         <ul>
           <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then Test Component <em>click Home</em> Back to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically With HerokuApp, So client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically With Heroku, So client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
+          <li><strong>Efficient production builds</strong>. In production mode, development-time and Local Server features are disabled, and <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
         <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. Open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code> and <code>npm start</code>.</p>
       </div>
