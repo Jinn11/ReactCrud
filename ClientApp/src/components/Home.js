0f470f0f6d1133d2274b6832b5a7dff8662d1ff2
Student <jsitbon@qcc.cuny.edu>
2019-08-08 14:25:59 -0400

    Build

--- ClientApp/src/components/Home.js
+++ ClientApp/src/components/Home.js
@@ -7,19 +7,19 @@ export class Home extends Component {
     return (
       <div>
         <h1>HELLO, Google !</h1>
-        <p>Welcome to new single-page application, built with:</p>
+        <p>Welcome to Single-Page Web App application, built with:</p>
         <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://www.javascript.com/'>JavaScript</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
+          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://www.javascript.com/'>JavaScript</a> for Cross-Platform server-side code</li>
+          <li><a href='https://facebook.github.io/react/'>React</a> for Client-side code</li>
           <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
         </ul>
         <p>To help get started, we have also set up:</p>
         <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then Test Component <em>click Home</em>Back to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
+          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then Test Component <em>click Home</em> Back to return here.</li>
+          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically With HerokuApp, So client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
+          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
         </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. Open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. Open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code> and <code>npm start</code>.</p>
       </div>
     );
   }
